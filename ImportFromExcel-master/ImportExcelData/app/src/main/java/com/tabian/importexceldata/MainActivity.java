84fbebd44a17cc3a54e4a9dd2e5136b3163cd4fd
Junqi Zhao <gcgl09zjq@gmail.com>
2019-01-14 16:08:00 -0500

    Initial commit

---
+++ ImportFromExcel-master/ImportExcelData/app/src/main/java/com/tabian/importexceldata/MainActivity.java
@@ -0,0 +1,361 @@
+package com.tabian.importexceldata;
+
+import android.Manifest;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    // Declare variables
+    private String[] FilePathStrings;
+    private String[] FileNameStrings;
+    private File[] listFile;
+    File file;
+
+    Button btnUpDirectory,btnSDCard;
+
+    ArrayList<String> pathHistory;
+    String lastDirectory;
+    int count = 0;
+
+    ArrayList<XYValue> uploadData;
+
+    ListView lvInternalStorage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvInternalStorage = (ListView) findViewById(R.id.lvInternalStorage);
+        btnUpDirectory = (Button) findViewById(R.id.btnUpDirectory);
+        btnSDCard = (Button) findViewById(R.id.btnViewSDCard);
+        uploadData = new ArrayList<>();
+
+        //need to check the permissions
+        checkFilePermissions();
+
+        lvInternalStorage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                lastDirectory = pathHistory.get(count);
+                if(lastDirectory.equals(adapterView.getItemAtPosition(i))){
+                    Log.d(TAG, "lvInternalStorage: Selected a file for upload: " + lastDirectory);
+
+                    //Execute method for reading the excel data.
+                    readExcelData(lastDirectory);
+
+                }else
+                {
+                    count++;
+                    pathHistory.add(count,(String) adapterView.getItemAtPosition(i));
+                    checkInternalStorage();
+                    Log.d(TAG, "lvInternalStorage: " + pathHistory.get(count));
+                }
+            }
+        });
+
+        //Goes up one directory level
+        btnUpDirectory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(count == 0){
+                    Log.d(TAG, "btnUpDirectory: You have reached the highest level directory.");
+                }else{
+                    pathHistory.remove(count);
+                    count--;
+                    checkInternalStorage();
+                    Log.d(TAG, "btnUpDirectory: " + pathHistory.get(count));
+                }
+            }
+        });
+
+        //Opens the SDCard or phone memory
+        btnSDCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = 0;
+                pathHistory = new ArrayList<String>();
+                pathHistory.add(count,System.getenv("EXTERNAL_STORAGE"));
+                Log.d(TAG, "btnSDCard: " + pathHistory.get(count));
+                checkInternalStorage();
+            }
+        });
+
+    }
+
+    /**
+     *reads the excel file columns then rows. Stores data as ExcelUploadData object
+     * @return
+     */
+    private void readExcelData(String filePath) {
+        Log.d(TAG, "readExcelData: Reading Excel File.");
+
+        //decarle input file
+        File inputFile = new File(filePath);
+
+        try {
+            InputStream inputStream = new FileInputStream(inputFile);
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            int rowsCount = sheet.getPhysicalNumberOfRows();
+            FormulaEvaluator formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();
+            StringBuilder sb = new StringBuilder();
+
+            //outter loop, loops through rows
+            for (int r = 1; r < rowsCount; r++) {
+                Row row = sheet.getRow(r);
+                int cellsCount = row.getPhysicalNumberOfCells();
+                //inner loop, loops through columns
+                for (int c = 0; c < cellsCount; c++) {
+                    //handles if there are to many columns on the excel sheet.XYValue
+                    String value = getCellAsString(row, c, formulaEvaluator);
+                    String cellInfo = "r:" + r + "; c:" + c + "; v:" + value;
+                    Log.d(TAG, "readExcelData: Data from row: " + cellInfo);
+                    sb.append(value + ", ");
+
+                }
+                sb.append(":");
+            }
+            Log.d(TAG, "readExcelData: STRINGBUILDER: " + sb.toString());
+
+            parseStringBuilder(sb);
+
+        }catch (FileNotFoundException e) {
+            Log.e(TAG, "readExcelData: FileNotFoundException. " + e.getMessage() );
+        } catch (IOException e) {
+            Log.e(TAG, "readExcelData: Error reading inputstream. " + e.getMessage() );
+        }
+    }
+
+    /**
+     * Method for parsing imported data and storing in ArrayList<XYValue>
+     */
+    public void parseStringBuilder(StringBuilder mStringBuilder){
+        Log.d(TAG, "parseStringBuilder: Started parsing.");
+
+        // splits the sb into rows.
+        String[] rows = mStringBuilder.toString().split(":");
+
+        //Add to the ArrayList<XYValue> row by row
+        for(int i=0; i<rows.length; i++) {
+            //Split the columns of the rows
+            String[] columns = rows[i].split(",");
+
+            //use try catch to make sure there are no "" that try to parse into doubles.
+            try{
+                double x = Double.parseDouble(columns[0]);
+                double y1 = Double.parseDouble(columns[1]);
+                double y2 = Double.parseDouble(columns[2]);
+                double y3 = Double.parseDouble(columns[3]);
+                double y4 = Double.parseDouble(columns[4]);
+                double y5 = Double.parseDouble(columns[5]);
+                double y6 = Double.parseDouble(columns[6]);
+                double y7 = Double.parseDouble(columns[7]);
+                double y8 = Double.parseDouble(columns[8]);
+                double y9 = Double.parseDouble(columns[9]);
+                double y10 = Double.parseDouble(columns[10]);
+                double y11 = Double.parseDouble(columns[11]);
+                double y12 = Double.parseDouble(columns[12]);
+                double y13 = Double.parseDouble(columns[13]);
+                double y14 = Double.parseDouble(columns[14]);
+                double y15 = Double.parseDouble(columns[15]);
+                double y16 = Double.parseDouble(columns[16]);
+                double y17 = Double.parseDouble(columns[17]);
+                double y18 = Double.parseDouble(columns[18]);
+                double y19 = Double.parseDouble(columns[19]);
+                double y20 = Double.parseDouble(columns[20]);
+                double y21 = Double.parseDouble(columns[21]);
+                double y22 = Double.parseDouble(columns[22]);
+                double y23 = Double.parseDouble(columns[23]);
+                double y24 = Double.parseDouble(columns[24]);
+                double y25 = Double.parseDouble(columns[25]);
+                double y26 = Double.parseDouble(columns[26]);
+                double y27 = Double.parseDouble(columns[27]);
+                double y28 = Double.parseDouble(columns[28]);
+                double y29 = Double.parseDouble(columns[29]);
+                double y30 = Double.parseDouble(columns[30]);
+                String cellInfo = "(x,y30): (" + x + "," + y30 + ")"; //print the first and last elements in the array
+                Log.d(TAG, "ParseStringBuilder: Data from row: " + cellInfo);
+
+                //add the the uploadData ArrayList
+                uploadData.add(new XYValue(x, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10, y11, y12, y13, y14, y15, y16, y17, y18, y19, y20, y21, y22, y23, y24, y25, y26, y27, y28, y29, y30));
+
+            }catch (NumberFormatException e){
+
+                Log.e(TAG, "parseStringBuilder: NumberFormatException: " + e.getMessage());
+
+            }
+        }
+
+        printDataToLog();
+    }
+
+    private void printDataToLog() {
+        Log.d(TAG, "printDataToLog: Printing data to log...");
+
+        for(int i = 0; i< uploadData.size(); i++){
+            double x = uploadData.get(i).getX();
+            double y30 = uploadData.get(i).getY30();
+            Log.d(TAG, "printDataToLog: (x,y) from loaded data: (" + x + "," + y30 + ")");
+        }
+    }
+
+    /**
+     * Returns the cell as a string from the excel file
+     * @param row
+     * @param c
+     * @param formulaEvaluator
+     * @return
+     */
+    private String getCellAsString(Row row, int c, FormulaEvaluator formulaEvaluator) {
+        String value = "";
+        try {
+            Cell cell = row.getCell(c);
+            CellValue cellValue = formulaEvaluator.evaluate(cell);
+            switch (cellValue.getCellType()) {
+                case Cell.CELL_TYPE_BOOLEAN:
+                    value = ""+cellValue.getBooleanValue();
+                    break;
+                case Cell.CELL_TYPE_NUMERIC:
+                    double numericValue = cellValue.getNumberValue();
+                    if(HSSFDateUtil.isCellDateFormatted(cell)) {
+                        double date = cellValue.getNumberValue();
+                        SimpleDateFormat formatter =
+                                new SimpleDateFormat("MM/dd/yy");
+                        value = formatter.format(HSSFDateUtil.getJavaDate(date));
+                    } else {
+                        value = ""+numericValue;
+                    }
+                    break;
+                case Cell.CELL_TYPE_STRING:
+                    value = ""+cellValue.getStringValue();
+                    break;
+                default:
+            }
+        } catch (NullPointerException e) {
+
+            Log.e(TAG, "getCellAsString: NullPointerException: " + e.getMessage() );
+        }
+        return value;
+    }
+
+    private void checkInternalStorage() {
+        Log.d(TAG, "checkInternalStorage: Started.");
+        try{
+            if (!Environment.getExternalStorageState().equals(
+                    Environment.MEDIA_MOUNTED)) {
+                toastMessage("No SD card found.");
+            }
+            else{
+                // Locate the image folder in your SD Car;d
+                file = new File(pathHistory.get(count));
+                Log.d(TAG, "checkInternalStorage: directory path: " + pathHistory.get(count));
+            }
+
+            listFile = file.listFiles();
+
+            // Create a String array for FilePathStrings
+            FilePathStrings = new String[listFile.length];
+
+            // Create a String array for FileNameStrings
+            FileNameStrings = new String[listFile.length];
+
+            for (int i = 0; i < listFile.length; i++) {
+                // Get the path of the image file
+                FilePathStrings[i] = listFile[i].getAbsolutePath();
+                // Get the name image file
+                FileNameStrings[i] = listFile[i].getName();
+            }
+
+            for (int i = 0; i < listFile.length; i++)
+            {
+                Log.d("Files", "FileName:" + listFile[i].getName());
+            }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FilePathStrings);
+            lvInternalStorage.setAdapter(adapter);
+
+        }catch(NullPointerException e){
+            Log.e(TAG, "checkInternalStorage: NULLPOINTEREXCEPTION " + e.getMessage() );
+        }
+    }
+
+    private void checkFilePermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
+            if (permissionCheck != 0) {
+
+                this.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, 1001); //Any number
+            }
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
+    }
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
